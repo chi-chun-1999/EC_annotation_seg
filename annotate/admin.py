105cdce4b1677f69ac482c4330b8cabf6dd811cd
chi-chun <jsutin11223350@gmai.com>
2023-10-25 13:48:45 +0800

    順利的建立dataset的基礎架構，不過目前還在測試這個架構的可行性

--- annotate/admin.py
+++ annotate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from annotate.models import SourceData, Task
+
+admin.site.register(SourceData)
+admin.site.register(Task)
+
--- annotate/models.py
+++ annotate/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class SourceData(models.Model):
+    id = models.AutoField(primary_key=True)
+    file_num = models.PositiveBigIntegerField()
+    file_dir = models.CharField(max_length=100)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    created_time = models.DateTimeField(auto_now_add=True)
+    source_data = models.OneToOneField(SourceData,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class ImageData(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    file_name = models.CharField(max_length=100)
+    research_id = models.CharField(max_length=100)
+    frame_num_from_ori = models.PositiveBigIntegerField()
+    source_data = models.ForeignKey(SourceData,on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file_name
+
+class AnnotationData(models.Model):
+
+    VIEW_CHOICES = (
+        ('2ch','2ch'),
+        ('3ch','3ch'),
+        ('4ch','4ch'),
+        ('other','other')
+    )
+
+    id = models.AutoField(primary_key=True)
+    image_data = models.OneToOneField(ImageData,on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    view = models.CharField(max_length=10)
+    key_points = models.JSONField()
+
+    def __str__(self):
+        return self.image_data.file_name
+
+
+
+
+class Polygons(models.Model):
+    AREA_CHOICES = (
+        ('LV','Left Ventricle'),
+        ('LVM','Left Ventricular Myocardium'),
+        ('LA','Left Atrium'),
+        ('LAM','Left Atrial Myocardium'),
+        ('other','other'),
+    )
+    area = models.CharField(max_length=10,choices=AREA_CHOICES)
+    annotation_data = models.ForeignKey(AnnotationData,on_delete=models.CASCADE)
+    points = models.JSONField()
+
+    class Meta:
+        ordering = ['area']
+    
+    def __str__(self):
+        return self.area
+
+    
