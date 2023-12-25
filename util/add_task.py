840f7b6b69cb975e6277dd942ac7f0abc542d62e
chi-chun <justin11223350@gmail.com>
2023-12-25 17:28:37 +0800

    加入新的資料

--- util/add_task.py
+++ util/add_task.py
@@ -24,9 +24,9 @@ def create_annotation(task_name):
 
 
 
-tasks = ['chi-chun-a3c-2']
-sub_dir = ['chi-chun-a3c-2']
-view = ['3ch']
+tasks = ['chi-chun-a2c-2','chi-chun-a3c-2','chi-chun-a4c']
+sub_dir = ['chi-chun-a2c-2','chi-chun-a3c-2','chi-chun-a4c']
+view = ['2ch','3ch','4ch']
 
 for i in range(len(tasks)):
     add_task(sub_dir[i],tasks[i],view[i])
